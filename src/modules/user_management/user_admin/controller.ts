523a72ddb2ce6d9eb3f9e994d085880b206c3ed7
Md-shefat-masum <mshefat924@gmail.com>
2024-04-21 23:35:03 +0600

    remove unused variables

--- src/modules/user_management/user_admin/controller.ts
+++ src/modules/user_management/user_admin/controller.ts
@@ -1,10 +1,9 @@
-'use strict'
-import { FastifyReply, FastifyRequest, FastifyInstance } from "fastify";
-import all from "./services/all"
+'use strict';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import all from './services/all';
 
 export default {
     all: async function (req: FastifyRequest, res: FastifyReply) {
-
         // console.log(this);
         // (this as any).print('ok')
         let users = await all();
@@ -19,27 +18,15 @@ export default {
         res.code(200).send(req.body);
     },
 
-    update: async function (req: FastifyRequest, res: FastifyReply) {
+    update: async function (req: FastifyRequest, res: FastifyReply) {},
 
-    },
+    soft_delete: async function (req: FastifyRequest, res: FastifyReply) {},
 
-    soft_delete: async function (req: FastifyRequest, res: FastifyReply) {
+    restore: async function (req: FastifyRequest, res: FastifyReply) {},
 
-    },
+    destroy: async function (req: FastifyRequest, res: FastifyReply) {},
 
-    restore: async function (req: FastifyRequest, res: FastifyReply) {
+    export: async function (req: FastifyRequest, res: FastifyReply) {},
 
-    },
-
-    destroy: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-
-    export: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-
-    import: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-}
+    import: async function (req: FastifyRequest, res: FastifyReply) {},
+};
