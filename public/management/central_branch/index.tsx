93b21f0b213af6344e5548fbb8c9da2bf6714c26
Md-shefat-masum <mshefat924@gmail.com>
2024-04-22 00:14:36 +0600

    fix prettier

--- public/management/central_branch/index.tsx
+++ public/management/central_branch/index.tsx
@@ -1,22 +1,7 @@
-function t(a: string, b: string): string [] {
-    let c: string[] = [
-        a,
-        b,
-    ];
-
-    return c;
-}
-
-let m = t("one", "two");
-console.log(m);
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-    RouterProvider,
-    createHashRouter,
-} from "react-router-dom";
-import { Provider } from 'react-redux'
+// import { RouterProvider, createHashRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 // import store from './store/index.js';
 // import dashboard_routes from './routes/dashboard_routes.js';
 
@@ -25,14 +10,14 @@ function Component() {
     //     dashboard_routes,
     // ]);
     // return <RouterProvider router={router}></RouterProvider>
-    return <h1>hi</h1>
+    return <h1>hi</h1>;
 }
 
 const container: HTMLElement | null = document.getElementById('app');
-if(container){
+if (container) {
     const root = createRoot(container); // createRoot(container!) if you use TypeScript
     root.render(
-        <Component></Component>
+        <Component></Component>,
         // <Provider store={store}>
         //     <Component />
         // </Provider>
