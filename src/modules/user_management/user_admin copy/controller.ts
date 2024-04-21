0860c4ba24c9d374570dc603e75d8aa0af070a17
Md-shefat-masum <mshefat924@gmail.com>
2024-04-21 23:35:32 +0600

    remove unused variables

--- src/modules/user_management/user_admin copy/controller.ts
+++ src/modules/user_management/user_admin copy/controller.ts
@@ -1,6 +1,6 @@
-'use strict'
-import { FastifyReply, FastifyRequest, FastifyInstance } from "fastify";
-import { sequelize, sequelize_response } from "../../../bootstrap/db.sql";
+'use strict';
+import { FastifyReply, FastifyRequest, FastifyInstance } from 'fastify';
+import { sequelize, sequelize_response } from '../../../bootstrap/db.sql';
 
 export default {
     all: async function (req: FastifyRequest, res: FastifyReply) {
@@ -9,10 +9,9 @@ export default {
         // let dbs: sequelize_response;
         // dbs = await sequelize();
         // users = await (dbs.db['user_admin'] as any).findAll();
-        
+
         res.code(200).send({ title: 'all', users });
         // console.log(users);
-
     },
     find: async function (req: FastifyRequest, res: FastifyReply) {
         res.code(200).send({ title: 'find' });
@@ -20,22 +19,10 @@ export default {
     store: async function (req: FastifyRequest, res: FastifyReply) {
         res.code(200).send(req.body);
     },
-    update: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-    soft_delete: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-    restore: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-    destroy: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-    export: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-    import: async function (req: FastifyRequest, res: FastifyReply) {
-
-    },
-}
+    update: async function (req: FastifyRequest, res: FastifyReply) {},
+    soft_delete: async function (req: FastifyRequest, res: FastifyReply) {},
+    restore: async function (req: FastifyRequest, res: FastifyReply) {},
+    destroy: async function (req: FastifyRequest, res: FastifyReply) {},
+    export: async function (req: FastifyRequest, res: FastifyReply) {},
+    import: async function (req: FastifyRequest, res: FastifyReply) {},
+};
