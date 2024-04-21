9b899fd0d69556a800a7e0360d754e643ac67611
Md-shefat-masum <mshefat924@gmail.com>
2024-04-21 23:35:37 +0600

    remove unused variables

--- src/modules/user_management/user_admin copy/routes.ts
+++ src/modules/user_management/user_admin copy/routes.ts
@@ -1,9 +1,11 @@
-'use strict'
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import controller from "./controller";
+'use strict';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import controller from './controller';
 
-module.exports = async function (fastify: FastifyInstance, opts: { prefix: '', db: {} }) {
-
+module.exports = async function (
+    fastify: FastifyInstance,
+    opts: { prefix: ''; db: {} },
+) {
     let prefix: string = '/admin-users-copy';
 
     fastify
@@ -14,6 +16,5 @@ module.exports = async function (fastify: FastifyInstance, opts: { prefix: '', d
         .post(`${prefix}/soft-delete`, controller.soft_delete)
         .post(`${prefix}/restore`, controller.restore)
         .post(`${prefix}/destroy`, controller.destroy)
-        .post(`${prefix}/import`, controller.import)
-
-}
+        .post(`${prefix}/import`, controller.import);
+};
