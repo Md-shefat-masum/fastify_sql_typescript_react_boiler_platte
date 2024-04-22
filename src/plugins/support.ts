69c94574cd968f1b9ac4317acfeb8b9f0d45cf37
Md-shefat-masum <mshefat924@gmail.com>
2024-04-22 14:58:32 +0600

    type fixed

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -1,3 +1,4 @@
+import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 
 export interface SupportPluginOptions {
@@ -6,11 +7,14 @@ export interface SupportPluginOptions {
 
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
-export default fp<SupportPluginOptions>(async (fastify) => {
-    fastify.decorate('someSupport', function () {
-        return 'hugs';
-    });
-});
+export default fp<SupportPluginOptions>(
+    (fastify: FastifyInstance, opts = {}, done) => {
+        fastify.decorate('someSupport', function () {
+            return 'hugs';
+        });
+        done();
+    },
+);
 
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
