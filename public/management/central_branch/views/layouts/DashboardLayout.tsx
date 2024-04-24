0c96fcd5941d07c0ca2a1efd46566eaa6f376b9b
Md-shefat-masum <mshefat924@gmail.com>
2024-04-25 03:32:49 +0600

    dashbaord layout design

--- public/management/central_branch/views/layouts/DashboardLayout.tsx
+++ public/management/central_branch/views/layouts/DashboardLayout.tsx
@@ -1,45 +1,17 @@
 import React, { useState } from 'react';
 import CommonAppWindow from '../components/CommonAppWindow';
 import AppNav from './shared/AppNav';
+import { Outlet } from 'react-router-dom';
 
 export interface Props {}
 
 const DashboardLayout: React.FC<Props> = (props: Props) => {
-    const [left, setleft] = useState(100);
-    const [top, settop] = useState(100);
-    const [height, setheight] = useState(100);
-    const [width, setwidth] = useState(100);
-    const [rotateAngle, setrotateAngle] = useState(0);
-
-    const handleResize = (style, isShiftKey, type) => {
-        // type is a string and it shows which resize-handler you clicked
-        // e.g. if you clicked top-right handler, then type is 'tr'
-        let { top, left, width, height } = style;
-        top = Math.round(top);
-        left = Math.round(left);
-        width = Math.round(width);
-        height = Math.round(height);
-
-        setleft(left);
-        settop(top);
-        setwidth(width);
-        setheight(height);
-    };
-
-    const handleRotate = (rotateAngle) => {
-        setrotateAngle(rotateAngle);
-    };
-
-    const handleDrag = (deltaX, deltaY) => {
-        setleft(left + deltaX);
-        settop(top + deltaY);
-    };
-
     return (
         <div>
             <div className="app_body">
                 <div className="app_window">
                     {/* <CommonAppWindow></CommonAppWindow> */}
+                    <Outlet></Outlet>
                 </div>
                 <div>
                     <div className="home_time_and_date">
