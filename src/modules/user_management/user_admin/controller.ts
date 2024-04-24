862e04827cbc2bde2a8f97945c76be74d31f5e1b
Md-shefat-masum <mshefat924@gmail.com>
2024-04-25 03:37:35 +0600

    controller setup

--- src/modules/user_management/user_admin/controller.ts
+++ src/modules/user_management/user_admin/controller.ts
@@ -1,26 +1,37 @@
 'use strict';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import all from './services/all';
+import details from './services/details';
+import soft_delete from './services/soft_delete';
+import store from './services/store';
 
 export default {
     all: async function (req: FastifyRequest, res: FastifyReply) {
         // console.log(this);
-        // (this as any).print('ok')
-        let users = await all();
-        res.code(200).send({ title: 'all', users });
+        let fastify_instance = this as any;
+        let data = await all(fastify_instance, req);
+        res.code(200).send(data);
     },
 
     find: async function (req: FastifyRequest, res: FastifyReply) {
-        res.code(200).send({ title: 'find' });
+        let fastify_instance = this as any;
+        let data = await details(fastify_instance, req);
+        res.code(200).send(data);
     },
 
     store: async function (req: FastifyRequest, res: FastifyReply) {
-        res.code(200).send(req.body);
+        let fastify_instance = this as any;
+        let data = await store(fastify_instance, req);
+        res.code(200).send(data);
     },
 
     update: async function (req: FastifyRequest, res: FastifyReply) {},
 
-    soft_delete: async function (req: FastifyRequest, res: FastifyReply) {},
+    soft_delete: async function (req: FastifyRequest, res: FastifyReply) {
+        let fastify_instance = this as any;
+        let data = await soft_delete(fastify_instance, req);
+        res.code(200).send(data);
+    },
 
     restore: async function (req: FastifyRequest, res: FastifyReply) {},
 
