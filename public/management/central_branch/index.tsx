5e5c794fbc35196c9b00596ea4c15744224730fd
Md-shefat-masum <mshefat924@gmail.com>
2024-04-25 03:31:49 +0600

    index page design

--- public/management/central_branch/index.tsx
+++ public/management/central_branch/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import store from './store/index.js';
+import { Provider } from 'react-redux';
+import store from './store/index.js';
 import dashboard_routes from './routes';
 
 function Component() {
@@ -14,9 +14,9 @@ const container: HTMLElement | null = document.getElementById('app');
 if (container) {
     const root = createRoot(container);
     root.render(
-        <Component></Component>,
-        // <Provider store={store}>
-        //     <Component />
-        // </Provider>
+        // <Component></Component>,
+        <Provider store={store}>
+            <Component />
+        </Provider>,
     );
 }
