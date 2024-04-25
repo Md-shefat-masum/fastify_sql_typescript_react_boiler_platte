3b224dc7a9ab65f27af88ca200702cb9c9eaeb9f
Md-shefat-masum <mshefat924@gmail.com>
2024-04-25 06:40:11 +0600

    create reducers

--- public/management/central_branch/views/pages/user_branch_staff/config/store/reducers.ts
+++ public/management/central_branch/views/pages/user_branch_staff/config/store/reducers.ts
@@ -1,9 +1,24 @@
 import { PayloadAction } from '@reduxjs/toolkit';
+import store, { useAppDispatch } from '../../../../../store';
+import { all } from './async_actions/all';
+
 export const store_reducers = {
     set_is_loading: (state, action: PayloadAction<boolean>) => {
         state.is_loading = action.payload;
     },
     set_loading_text: (state, action: PayloadAction<string>) => {
         state.loading_text = action.payload;
+    },
+    set_all: (state, action: PayloadAction<string>) => {
+        state.all = action.payload;
+    },
+    set_page: (state, action: PayloadAction<string>) => {
+        state.page = action.payload;
+    },
+    set_url: (state, action: PayloadAction<string>) => {
+        state.url = action.payload;
+    },
+    set_paginate: (state, action: PayloadAction<string>) => {
+        state.paginate = action.payload;
     },
 };
