c1295f5a221b857e2079e27d113e49c5edd79c3b
Md-shefat-masum <mshefat924@gmail.com>
2024-04-25 06:41:12 +0600

    seeder test

--- src/modules/user_management/user_admin/models/seeders/20240419174508-user_admin.js
+++ src/modules/user_management/user_admin/models/seeders/20240419174508-user_admin.js
@@ -2,33 +2,38 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
 
-    await queryInterface.bulkInsert('user_admins', [{
-      first_name: 'John Doe',
-      lastN_name: 'John Doe',
-      email: 'John Doe',
-      created_at: '2024-02-01 11:45:01',
-      updated_at: '2024-02-01 11:45:01'
-    }], {});
-  },
+        let users = [];
+        ['selim', 'karim', 'sahed', 'tarek', 'shafiq'].forEach(async (i) => {
+            users.push({
+                name: i,
+                preferred_name: i,
+                status: 1,
+                created_at: '2024-02-01 11:45:01',
+                updated_at: '2024-02-01 11:45:01',
+            });
+        });
 
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    await queryInterface.bulkDelete('user_admins', null, {});
-  }
+        await queryInterface.bulkInsert('user_models', users, {});
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('user_models', null, {});
+    },
 };
