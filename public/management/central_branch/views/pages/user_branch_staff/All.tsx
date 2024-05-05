6815374bc32c773fa9835699a16ba91c72f8c62a
Md-shefat-masum <mshefat924@gmail.com>
2024-05-05 19:58:40 +0600

    setup all data management

--- public/management/central_branch/views/pages/user_branch_staff/All.tsx
+++ public/management/central_branch/views/pages/user_branch_staff/All.tsx
@@ -11,6 +11,9 @@ import Paginate from '../../components/Paginate';
 import Filter from './components/canvas/Filter';
 import QuickView from './components/canvas/QuickView';
 import storeSlice from './config/store';
+import { anyObject } from '../../../common_types/object';
+import TableRow from './components/all_data_page/TableRow';
+import TableRowAction from './components/all_data_page/TableRowAction';
 
 export interface Props {}
 
@@ -24,6 +27,11 @@ const All: React.FC<Props> = (props: Props) => {
     useEffect(() => {
         dispatch(all({}) as any);
     }, []);
+
+    function quick_view(data: anyObject = {}) {
+        dispatch(storeSlice.actions.set_item(data));
+        dispatch(storeSlice.actions.set_show_quick_view_canvas(true));
+    }
 
     return (
         <div className="page_content">
@@ -42,7 +50,10 @@ const All: React.FC<Props> = (props: Props) => {
                                     <tr>
                                         <th />
                                         <th>
-                                            <input type="checkbox" />
+                                            <input
+                                                type="checkbox"
+                                                className="form_checkbox"
+                                            />
                                         </th>
                                         <th>ID</th>
                                         <th>First Name</th>
@@ -51,47 +62,48 @@ const All: React.FC<Props> = (props: Props) => {
                                     </tr>
                                 </thead>
                                 <tbody id="all_list">
-                                    {(state.all as any).data?.map((i) => {
-                                        return (
-                                            <tr id="21" key={i.id} className="">
-                                                <td>
-                                                    <span className="icon" />
-                                                    <div className="table_action_btns">
-                                                        <ul>
-                                                            <li>
-                                                                <a href="/user/21">
-                                                                    Show
-                                                                </a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">
-                                                                    Edit
-                                                                </a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">
-                                                                    Delete
-                                                                </a>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <input type="checkbox" />
-                                                </td>
-                                                <td>{i.id}</td>
-                                                <td>{i.name}</td>
-                                                <td>{i.preferred_name}</td>
-                                                <td>
-                                                    <img
-                                                        src="/assets/dashboard/images/avatar.png"
-                                                        alt=""
-                                                        style={{ height: 30 }}
-                                                    />
-                                                </td>
-                                            </tr>
-                                        );
-                                    })}
+                                    {(state.all as any)?.data?.map(
+                                        (i: { [key: string]: any }) => {
+                                            return (
+                                                <tr
+                                                    key={i.id}
+                                                    className="table_rows"
+                                                >
+                                                    <td>
+                                                        <TableRowAction
+                                                            item={i}
+                                                        ></TableRowAction>
+                                                    </td>
+                                                    <td>
+                                                        <input
+                                                            className="form_checkbox"
+                                                            type="checkbox"
+                                                        />
+                                                    </td>
+                                                    <td>{i.id}</td>
+                                                    <td>
+                                                        <span
+                                                            onClick={() =>
+                                                                quick_view(i)
+                                                            }
+                                                        >
+                                                            {i.name}
+                                                        </span>
+                                                    </td>
+                                                    <td>{i.preferred_name}</td>
+                                                    <td>
+                                                        <img
+                                                            src="/assets/dashboard/images/avatar.png"
+                                                            alt=""
+                                                            style={{
+                                                                height: 30,
+                                                            }}
+                                                        />
+                                                    </td>
+                                                </tr>
+                                            );
+                                        },
+                                    )}
                                 </tbody>
                             </table>
                         </div>
@@ -99,6 +111,7 @@ const All: React.FC<Props> = (props: Props) => {
                         <Paginate
                             set_url={storeSlice.actions.set_url}
                             set_paginate={storeSlice.actions.set_paginate}
+                            set_page={storeSlice.actions.set_page}
                             all={all}
                             data={state.all as any}
                             selected_paginate={state.paginate}
