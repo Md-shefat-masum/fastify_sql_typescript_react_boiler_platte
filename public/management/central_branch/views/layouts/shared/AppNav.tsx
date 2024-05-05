02db665b4e6e21196e79518fc3ef117bf089ca97
Md-shefat-masum <mshefat924@gmail.com>
2024-05-05 19:58:14 +0600

    cached memory show

--- public/management/central_branch/views/layouts/shared/AppNav.tsx
+++ public/management/central_branch/views/layouts/shared/AppNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppNavRight from './AppNavRight';
 
 export interface Props {}
 
@@ -290,47 +291,7 @@ const AppNav: React.FC<Props> = (props: Props) => {
                     </ul>
                 </div>
                 <div className="right">
-                    <ul className="navigation_list">
-                        <li className="icon_link_li">
-                            <a href="#dashboard#" className="navigation_link">
-                                <span className="material-symbols-outlined fill">
-                                    expand_less
-                                </span>
-                            </a>
-                        </li>
-                        <li className="icon_link_li">
-                            <a
-                                href="#dashboard#"
-                                onClick={() => ''}
-                                className="navigation_link"
-                            >
-                                <span className="material-symbols-outlined fill">
-                                    fullscreen
-                                </span>
-                            </a>
-                        </li>
-                        <li className="icon_link_li">
-                            <a href="#dashboard#" className="navigation_link">
-                                <span className="material-symbols-outlined fill">
-                                    settings
-                                </span>
-                            </a>
-                        </li>
-                        <li className="icon_link_li">
-                            <a href="#dashboard#" className="navigation_link">
-                                <span className="material-symbols-outlined fill">
-                                    wifi
-                                </span>
-                            </a>
-                        </li>
-                        <li className="nave_date_time">
-                            <a href="#dashboard#" className="">
-                                <span>01:18:56</span>
-                                <span className="meridian">am</span>
-                                <span> &nbsp; Mon 22 Apr</span>
-                            </a>
-                        </li>
-                    </ul>
+                    <AppNavRight></AppNavRight>
                 </div>
             </div>
         </>
