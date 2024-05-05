759f82e259d0270c7b93699071d0576e470f1439
Md-shefat-masum <mshefat924@gmail.com>
2024-05-05 20:02:53 +0600

    set api and store

--- src/modules/user_management/user_admin/services/store.ts
+++ src/modules/user_management/user_admin/services/store.ts
@@ -6,11 +6,14 @@ async function store(fastify_instance: any, req: FastifyRequest): Promise<{}> {
     let models = await db();
     let body = req.body as { [key: string]: string };
 
+    console.clear();
+    fastify_instance.print(body);
+
     let data: { [key: string]: any } = await models.User.create({
         name: body.name,
         preferred_name: body.preferred_name,
     });
-    console.log({ data });
+    // console.log({ data });
 
     if (data) {
         return data;
