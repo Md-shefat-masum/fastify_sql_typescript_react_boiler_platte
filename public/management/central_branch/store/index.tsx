d5fed621ab33663274d2624b3f6bb77646642e8a
Md-shefat-masum <mshefat924@gmail.com>
2024-05-05 19:57:21 +0600

    connect devtools

--- public/management/central_branch/store/index.tsx
+++ public/management/central_branch/store/index.tsx
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import user_branch_staff_slice from '../views/pages/user_branch_staff/config/store';
 import { useDispatch } from 'react-redux';
+
+import commonStore from './slices/common_slice';
+import user_branch_staff_slice from '../views/pages/user_branch_staff/config/store';
 
 const store = configureStore({
     reducer: {
         user_branch_staff: user_branch_staff_slice.reducer,
+        common_store: commonStore.reducer,
     },
+    devTools: true,
 });
 
 export type AppDispatch = typeof store.dispatch;
