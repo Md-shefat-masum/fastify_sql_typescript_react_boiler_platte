a553690e2ef5b78ca3a0964838abe2a31befb3a2
Md-shefat-masum <mshefat924@gmail.com>
2024-05-06 12:31:14 +0600

    fix services response types

--- src/modules/user_management/user_admin/services/all.ts
+++ src/modules/user_management/user_admin/services/all.ts
@@ -1,11 +1,13 @@
 import { Model } from 'sequelize';
 import db from '../models/db';
 import { FastifyRequest } from 'fastify';
+import response from '../helpers/response';
+import { responseObject } from '../../../common_types/object';
 
 async function all(
     fastify_instance: any,
     req: FastifyRequest,
-): Promise<Model[]> {
+): Promise<responseObject> {
     let models = await db();
     let query_param = req.query as any;
 
@@ -28,13 +30,17 @@ async function all(
 
     let paginate = parseInt((req.query as any).paginate);
 
-    let data = await fastify_instance.paginate(
-        req,
-        models.User,
-        paginate,
-        query,
-    );
-    return data;
+    try {
+        let data = await fastify_instance.paginate(
+            req,
+            models.User,
+            paginate,
+            query,
+        );
+        return response(200, 'data fetched', data);
+    } catch (error) {
+        return response(500, 'data fetching failed', { error });
+    }
 }
 
 export default all;
