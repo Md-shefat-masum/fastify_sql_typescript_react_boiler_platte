a14a172dea8ee73e0ddb875eb43f65374286dde6
Md-shefat-masum <mshefat924@gmail.com>
2024-05-07 03:06:17 +0600

    login serve

--- src/modules/auth_management/authetication/services/login.ts
+++ src/modules/auth_management/authetication/services/login.ts
@@ -2,11 +2,14 @@ import { FindAndCountOptions, Model } from 'sequelize';
 import db from '../models/db';
 import { FastifyInstance, FastifyRequest } from 'fastify';
 import response from '../helpers/response';
+var bcrypt = require('bcrypt');
+
 import {
     anyObject,
     responseObject,
     Request,
 } from '../../../common_types/object';
+import { env } from 'process';
 
 async function login(
     fastify_instance: FastifyInstance,
@@ -17,14 +20,35 @@ async function login(
 
     try {
         let data: anyObject | null = {};
+        let token: anyObject = {};
         if (body) {
             data = await models.User.findOne({
                 where: {
                     email: body.email,
                 },
             });
+
+            if (data) {
+                let check_pass = await bcrypt.compare(
+                    body.password,
+                    data.password,
+                );
+
+                if (check_pass) {
+                    let jwt = require('jsonwebtoken');
+                    const secretKey = env.JTI;
+                    let secret = Math.random().toString(36).substring(2, 10);
+                    token = await jwt.sign(
+                        { id: data.id, token: secret },
+                        secretKey,
+                    );
+
+                    data.token = secret;
+                    await data.save();
+                }
+            }
         }
-        return response(200, 'data fetched', data || {});
+        return response(200, 'data fetched', { token });
     } catch (error) {
         return response(500, 'data fetching failed', { error });
     }
