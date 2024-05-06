a7c162fb3deb412125e21cecf249cfa090c36811
Md-shefat-masum <mshefat924@gmail.com>
2024-05-07 02:03:34 +0600

    user managment route

--- src/modules/user_management/user_admin/routes.ts
+++ src/modules/user_management/user_admin/routes.ts
@@ -4,14 +4,15 @@ import controller from './controller';
 
 module.exports = async function (fastify: FastifyInstance) {
     let prefix: string = '/admin-users';
+    const controllerInstance = controller(fastify);
 
     fastify
-        .get(`${prefix}`, controller.all)
-        .get(`${prefix}/:id`, controller.find)
-        .post(`${prefix}/store`, controller.store)
-        .post(`${prefix}/update`, controller.update)
-        .post(`${prefix}/soft-delete`, controller.soft_delete)
-        .post(`${prefix}/restore`, controller.restore)
-        .post(`${prefix}/destroy`, controller.destroy)
-        .post(`${prefix}/import`, controller.import);
+        .get(`${prefix}`, controllerInstance.all)
+        .get(`${prefix}/:id`, controllerInstance.find)
+        .post(`${prefix}/store`, controllerInstance.store)
+        .post(`${prefix}/update`, controllerInstance.update)
+        .post(`${prefix}/soft-delete`, controllerInstance.soft_delete)
+        .post(`${prefix}/restore`, controllerInstance.restore)
+        .post(`${prefix}/destroy`, controllerInstance.destroy)
+        .post(`${prefix}/import`, controllerInstance.import);
 };
