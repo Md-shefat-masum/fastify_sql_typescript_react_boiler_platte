2ac0800ddbba3cf97ade351d5f3f77dc821c6c66
Md-shefat-masum <mshefat924@gmail.com>
2024-05-07 01:58:11 +0600

    common setup fixed

--- public/management/central_branch/views/pages/user_branch_staff/config/setup.ts
+++ public/management/central_branch/views/pages/user_branch_staff/config/setup.ts
@@ -14,6 +14,7 @@ const setup: setup_type = {
     layout_title: prefix + ' Management',
 
     all_page_title: 'All ' + prefix,
+    details_page_title: 'Details ' + prefix,
     create_page_title: 'Create ' + prefix,
     edit_page_title: 'Edit ' + prefix,
 };
