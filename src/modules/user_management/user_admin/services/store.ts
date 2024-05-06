4b2d9102bf550237ed74c00312a4f56b3275d105
Md-shefat-masum <mshefat924@gmail.com>
2024-05-06 12:31:42 +0600

    setup validate, data insert and response

--- src/modules/user_management/user_admin/services/store.ts
+++ src/modules/user_management/user_admin/services/store.ts
@@ -1,24 +1,62 @@
-import { Model } from 'sequelize';
 import db from '../models/db';
 import { FastifyRequest } from 'fastify';
+import { body, validationResult } from 'express-validator';
+import {
+    anyObject,
+    responseObject,
+    Request,
+} from '../../../common_types/object';
+import response from '../helpers/response';
+import { InferCreationAttributes } from 'sequelize';
 
-async function store(fastify_instance: any, req: FastifyRequest): Promise<{}> {
+async function validate(req: Request) {
+    await body('name')
+        .not()
+        .isEmpty()
+        .withMessage('the name field is required')
+        .run(req);
+
+    await body('preferred_name')
+        .not()
+        .isEmpty()
+        .withMessage('the preferred name field is required')
+        .run(req);
+
+    let result = await validationResult(req);
+
+    return result;
+}
+
+async function store(
+    fastify_instance: any,
+    req: FastifyRequest,
+): Promise<responseObject> {
+    /** validation */
+    let validate_result = await validate(req as Request);
+    if (!validate_result.isEmpty()) {
+        return response(422, 'validation error', validate_result.array());
+    }
+
+    /** initializations */
     let models = await db();
-    let body = req.body as { [key: string]: string };
+    let body = req.body as anyObject;
+    let data = new models.User();
 
-    console.clear();
-    fastify_instance.print(body);
-
-    let data: { [key: string]: any } = await models.User.create({
+    let inputs: InferCreationAttributes<typeof data> = {
         name: body.name,
         preferred_name: body.preferred_name,
-    });
-    // console.log({ data });
+    };
 
-    if (data) {
-        return data;
-    } else {
-        return {};
+    /** print request data into console */
+    // console.clear();
+    // fastify_instance.print(inputs);
+
+    /** store data into database */
+    try {
+        (await data.update(inputs)).save();
+        return response(200, 'data created', data);
+    } catch (error) {
+        return response(500, 'data creation error', { error });
     }
 }
 
