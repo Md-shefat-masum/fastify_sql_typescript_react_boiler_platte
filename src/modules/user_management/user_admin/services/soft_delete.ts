1059137ff9d875df42ba873611178620c126d10b
Md-shefat-masum <mshefat924@gmail.com>
2024-05-08 05:27:02 +0600

    error handling

--- src/modules/user_management/user_admin/services/soft_delete.ts
+++ src/modules/user_management/user_admin/services/soft_delete.ts
@@ -3,6 +3,8 @@ import db from '../models/db';
 import { FastifyInstance, FastifyRequest } from 'fastify';
 import { responseObject } from '../../../common_types/object';
 import response from '../helpers/response';
+import custom_error from '../helpers/custom_error';
+import error_trace from '../helpers/error_trace';
 
 async function soft_delete(
     fastify_instance: FastifyInstance,
@@ -11,20 +13,30 @@ async function soft_delete(
     let models = await db();
     let body = req.body as { [key: string]: any };
 
-    let data = await models.User.findOne({
-        where: {
-            id: body.id,
-        },
-    });
-
-    if (data) {
-        await data.update({
-            status: 0,
+    try {
+        let data = await models.User.findOne({
+            where: {
+                id: body.id,
+            },
         });
-        await data.save();
-        return response(200, 'data deactivated', data);
-    } else {
-        return response(500, 'data deactivation failed', {});
+
+        if (data) {
+            await data.update({
+                status: 0,
+            });
+            await data.save();
+            return response(200, 'data deactivated', data);
+        } else {
+            throw new custom_error('Forbidden', 403, 'operation not possible');
+        }
+    } catch (error: any) {
+        let uid = await error_trace(models, error, req.url, req.body);
+        if (error instanceof custom_error) {
+            error.uid = uid;
+        } else {
+            throw new custom_error('server error', 500, error.message, uid);
+        }
+        throw error;
     }
 }
 
