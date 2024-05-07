687c55b8f4281bf58986b5f8b6afe4d82fcde771
Md-shefat-masum <mshefat924@gmail.com>
2024-05-08 05:20:15 +0600

    setup error handling

--- src/modules/user_management/user_admin/services/details.ts
+++ src/modules/user_management/user_admin/services/details.ts
@@ -1,8 +1,9 @@
-import { Model } from 'sequelize';
 import db from '../models/db';
 import { FastifyInstance, FastifyRequest } from 'fastify';
 import { responseObject } from '../../../common_types/object';
 import response from '../helpers/response';
+import error_trace from '../helpers/error_trace';
+import custom_error from '../helpers/custom_error';
 
 async function details(
     fastify_instance: FastifyInstance,
@@ -17,13 +18,20 @@ async function details(
                 id: params.id,
             },
         });
+
         if (data) {
             return response(200, 'data created', data);
         } else {
-            return response(404, 'not found', {});
+            throw new custom_error('not found', 404, 'data not found');
         }
-    } catch (error) {
-        return response(500, 'data creation error', { error });
+    } catch (error: any) {
+        let uid = await error_trace(models, error, req.url, req.params);
+        if (error instanceof custom_error) {
+            error.uid = uid;
+        } else {
+            throw new custom_error('server error', 500, error.message, uid);
+        }
+        throw error;
     }
 }
 
