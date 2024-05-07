918dba5787c7e9ef03cb03feadb4dd03b0d11bee
Md-shefat-masum <mshefat924@gmail.com>
2024-05-08 05:27:16 +0600

    error handling

--- src/modules/user_management/user_admin/services/update.ts
+++ src/modules/user_management/user_admin/services/update.ts
@@ -8,6 +8,8 @@ import {
 } from '../../../common_types/object';
 import response from '../helpers/response';
 import { InferCreationAttributes } from 'sequelize';
+import custom_error from '../helpers/custom_error';
+import error_trace from '../helpers/error_trace';
 
 async function validate(req: Request) {
     await body('id')
@@ -65,12 +67,16 @@ async function update(
             await data.save();
             return response(200, 'data updated', data);
         } else {
-            return response(500, 'data update failed', {
-                error: 'data not found by given query',
-            });
+            throw new custom_error('Forbidden', 403, 'operation not possible');
         }
-    } catch (error) {
-        return response(500, 'data creation error', { error });
+    } catch (error: any) {
+        let uid = await error_trace(models, error, req.url, req.body);
+        if (error instanceof custom_error) {
+            error.uid = uid;
+        } else {
+            throw new custom_error('server error', 500, error.message, uid);
+        }
+        throw error;
     }
 }
 
