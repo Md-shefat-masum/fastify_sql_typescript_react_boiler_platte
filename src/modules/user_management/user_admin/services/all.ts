67da6410664524e0a7531112d28c1390f0e37bb5
Md-shefat-masum <mshefat924@gmail.com>
2024-05-08 05:19:53 +0600

    setup error handling

--- src/modules/user_management/user_admin/services/all.ts
+++ src/modules/user_management/user_admin/services/all.ts
@@ -3,6 +3,8 @@ import db from '../models/db';
 import { FastifyInstance, FastifyRequest } from 'fastify';
 import response from '../helpers/response';
 import { anyObject, responseObject } from '../../../common_types/object';
+import error_trace from '../helpers/error_trace';
+import custom_error from '../helpers/custom_error';
 
 async function all(
     fastify_instance: FastifyInstance,
@@ -56,10 +58,9 @@ async function all(
             query,
         );
         return response(200, 'data fetched', data);
-    } catch (error) {
-        console.log(error);
-
-        return response(500, 'data fetching failed', { error });
+    } catch (error: any) {
+        let uid = await error_trace(models, error, req.url, req.query);
+        throw new custom_error('server error', 500, error.message, uid);
     }
 }
 
