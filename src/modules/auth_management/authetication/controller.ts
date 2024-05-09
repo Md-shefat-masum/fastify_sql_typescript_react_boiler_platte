bfc4f398b6f576cde90e9b325499c7e8fe34ae26
Md-shefat-masum <mshefat924@gmail.com>
2024-05-10 02:14:18 +0600

    login and forget dynamic

--- src/modules/auth_management/authetication/controller.ts
+++ src/modules/auth_management/authetication/controller.ts
@@ -5,12 +5,26 @@ import login from './services/login';
 import register from './services/register';
 import forget from './services/forget';
 import auth_user from './services/auth_user';
+import logout from './services/logout';
+const { serialize, parse } = require('@fastify/cookie');
 
 export default function (fastify: FastifyInstance) {
     return {
         login: async function (req: FastifyRequest, res: FastifyReply) {
             let data: responseObject = await login(fastify, req);
-            res.code(200).send(data);
+
+            const cookie = serialize('token', 'Bearer ' + data.data.token, {
+                maxAge: 60_000,
+            });
+
+            res.header('Set-Cookie', cookie);
+            res.code(data.status).send(data);
+        },
+
+        logout: async function (req: FastifyRequest, res: FastifyReply) {
+            let data: responseObject = await logout(fastify, req);
+            res.clearCookie('token');
+            res.code(data.status).send(data);
         },
 
         auth_user: async function (req: FastifyRequest, res: FastifyReply) {
