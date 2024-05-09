2ab02a864d1a1fe14e23ee5480a01b328b605cf0
Md-shefat-masum <mshefat924@gmail.com>
2024-05-10 02:15:33 +0600

    middleware for public routes

--- src/modules/auth_management/authetication/services/check_auth_and_redirect.ts
+++ src/modules/auth_management/authetication/services/check_auth_and_redirect.ts
@@ -12,24 +12,17 @@ const check_auth_and_redirect = async (
     const token = request.headers.authorization;
 
     if (!token || !token.startsWith('Bearer ')) {
-        // reply.code(401).send({ error: 'Unauthorized' });
         return reply.redirect('/login');
     }
 
-    try {
-        const decoded = jwt.verify(token.slice(7), secretKey);
-        let models = await db();
-        let user = await models.User.findByPk(decoded.id);
-        if (user && user.token == decoded.token) {
-            (request as anyObject).user = decoded;
-            return;
-        } else {
-            reply.code(401).send({ error: 'Unauthorized' });
-            return;
-        }
-    } catch (error) {
-        reply.code(401).send({ error: 'Unauthorized' });
+    const decoded = jwt.verify(token.slice(7), secretKey);
+    let models = await db();
+    let user = await models.User.findByPk(decoded.id);
+    if (user && user.token == decoded.token) {
+        (request as anyObject).user = decoded;
         return;
+    } else {
+        return reply.redirect('/login');
     }
 };
 
