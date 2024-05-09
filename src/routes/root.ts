1a27b6dd6cf90d452d5fcc2f6ae6ac7bd5ffa472
Md-shefat-masum <mshefat924@gmail.com>
2024-05-09 22:17:48 +0600

    modify routes

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,11 +1,16 @@
 'use strict';
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import check_auth_and_redirect from '../modules/auth_management/authetication/services/check_auth_and_redirect';
 // const fs = require('node:fs');
 module.exports = async function (fastify: FastifyInstance) {
     fastify
-        .get('/', async (req: FastifyRequest, reply: FastifyReply) => {
-            return reply.view('index.ejs', { text: 'Hello EJS!' });
-        })
+        .get(
+            '/',
+            { preHandler: check_auth_and_redirect },
+            async (req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('index.ejs', { text: 'Hello EJS!' });
+            },
+        )
         .get('/login', async (req: FastifyRequest, reply: FastifyReply) => {
             return reply.view('login.ejs');
         })
@@ -17,32 +22,6 @@ module.exports = async function (fastify: FastifyInstance) {
         .post('/c', async function (req: FastifyRequest) {
             const { body, validationResult } = require('express-validator');
             console.log('request', req.body);
-
             return { a: 'sdf' };
-            // await body('title')
-            //     .not()
-            //     .isEmpty()
-            //     .withMessage('the title field is required')
-            //     .run(req);
-
-            // let result = validationResult(req);
-
-            // let bodys: {
-            //     'image[]': {
-            //         data: string;
-            //         name: string;
-            //         ext: string;
-            //     }[];
-            // } = (req as any).body;
-
-            // if (bodys['image[]'].length) {
-            //     // console.log(bodys['image[]'][0]);
-            //     fastify.upload(
-            //         bodys['image[]'][0],
-            //         'uploads/users/' + bodys['image[]'][0].name,
-            //     );
-            // }
-
-            // return { d: 'uploads/', errors: result.array() };
         });
 };
