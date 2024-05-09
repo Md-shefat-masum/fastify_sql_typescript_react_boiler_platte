11dc679c444b70fb7848318f10c3bb4f5c1403bc
Md-shefat-masum <mshefat924@gmail.com>
2024-05-10 02:15:43 +0600

    middle for api routes

--- src/modules/auth_management/authetication/services/check_auth.ts
+++ src/modules/auth_management/authetication/services/check_auth.ts
@@ -6,7 +6,9 @@ import { env } from 'process';
 const check_auth = async (request: FastifyRequest, reply: FastifyReply) => {
     const secretKey = env.JTI;
     const jwt = require('jsonwebtoken');
-    const token = request.headers.authorization;
+    // const token = request.headers.authorization;
+    const token = request.cookies.token;
+    const user_agent = request.headers['user-agent'];
 
     if (!token || !token.startsWith('Bearer ')) {
         reply.code(401).send({ error: 'Unauthorized' });
@@ -17,7 +19,11 @@ const check_auth = async (request: FastifyRequest, reply: FastifyReply) => {
         const decoded = jwt.verify(token.slice(7), secretKey);
         let models = await db();
         let user = await models.User.findByPk(decoded.id);
-        if (user && user.token == decoded.token) {
+        if (
+            user &&
+            user.token == decoded.token &&
+            decoded.user_agent == user_agent
+        ) {
             (request as anyObject).user = decoded;
             return;
         } else {
