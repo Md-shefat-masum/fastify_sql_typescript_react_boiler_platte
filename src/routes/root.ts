3b3a5d346ae65d50c6c5316e2b2eb1a314f6b113
Md-shefat-masum <mshefat924@gmail.com>
2024-05-10 02:16:40 +0600

    connect minifify view into login function

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,6 +1,7 @@
 'use strict';
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import check_auth_and_redirect from '../modules/auth_management/authetication/services/check_auth_and_redirect';
+import minified_view from '../helpers/minified_view';
 // const fs = require('node:fs');
 module.exports = async function (fastify: FastifyInstance) {
     fastify
@@ -12,7 +13,8 @@ module.exports = async function (fastify: FastifyInstance) {
             },
         )
         .get('/login', async (req: FastifyRequest, reply: FastifyReply) => {
-            return reply.view('login.ejs');
+            let html = await minified_view(fastify, 'login.ejs');
+            return reply.type('text/html').send(html);
         })
         .get('/about', async function (request, reply) {
             return (reply as any).cookie('baz', 'baz').view('about.ejs', {
